209f4fadcf88a96ea4457aca4163c77b5abc8bdd
Alex Ogden <61543258+alex-ogden@users.noreply.github.com>
2025-10-22 19:03:44 +0100

    Update cpu.rs

--- src/cpu.rs
+++ src/cpu.rs
@@ -33,17 +33,3 @@ impl Cpu {
 
     pub fn step(&mut self, mem: &mut Memory) -> u32 {}
 }
-
-pub struct Memory {
-    rom: Vec<u8>,
-    ram: Vec<u8>,
-}
-
-impl Memory {
-    pub fn new(rom: Vec<u8>) -> Self {
-        Self {
-            rom,
-            ram: vec![0; 0x2000], // 8K RAM
-        }
-    }
-}
