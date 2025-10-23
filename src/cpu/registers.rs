ca509e8f9910ce096c770726e0889255877a1e1b
Alex Ogden <alex.s.ogden@icloud.com>
2025-10-24 00:14:00 +0100

    making functions public

--- src/cpu/registers.rs
+++ src/cpu/registers.rs
@@ -3,56 +3,56 @@ use super::Cpu;
 // Register and flag helper methods
 impl Cpu {
     // == Flag-setting helper functions == //
-    fn set_flag_c(&mut self, val: bool) {
+    pub fn set_flag_c(&mut self, val: bool) {
         if val {
             self.f |= 0x01;
         } else {
             self.f &= !0x01;
         }
     }
-    fn set_flag_n(&mut self, val: bool) {
+    pub fn set_flag_n(&mut self, val: bool) {
         if val {
             self.f |= 0x02;
         } else {
             self.f &= !0x02;
         }
     }
-    fn set_flag_pv(&mut self, val: bool) {
+    pub fn set_flag_pv(&mut self, val: bool) {
         if val {
             self.f |= 0x04;
         } else {
             self.f &= !0x04;
         }
     }
-    fn set_flag_y(&mut self, val: bool) {
+    pub fn set_flag_y(&mut self, val: bool) {
         if val {
             self.f |= 0x08;
         } else {
             self.f &= !0x08;
         }
     }
-    fn set_flag_h(&mut self, val: bool) {
+    pub fn set_flag_h(&mut self, val: bool) {
         if val {
             self.f |= 0x10;
         } else {
             self.f &= !0x10;
         }
     }
-    fn set_flag_x(&mut self, val: bool) {
+    pub fn set_flag_x(&mut self, val: bool) {
         if val {
             self.f |= 0x20;
         } else {
             self.f &= !0x20;
         }
     }
-    fn set_flag_z(&mut self, val: bool) {
+    pub fn set_flag_z(&mut self, val: bool) {
         if val {
             self.f |= 0x40;
         } else {
             self.f &= !0x40;
         }
     }
-    fn set_flag_s(&mut self, val: bool) {
+    pub fn set_flag_s(&mut self, val: bool) {
         if val {
             self.f |= 0x80;
         } else {
@@ -60,55 +60,55 @@ impl Cpu {
         }
     }
     // == Flag-getting helper functions == //
-    fn get_flag_c(&self) -> bool {
+    pub fn get_flag_c(&self) -> bool {
         (self.f & 0x01) != 0
     }
-    fn get_flag_n(&self) -> bool {
+    pub fn get_flag_n(&self) -> bool {
         (self.f & 0x02) != 0
     }
-    fn get_flag_pv(&self) -> bool {
+    pub fn get_flag_pv(&self) -> bool {
         (self.f & 0x04) != 0
     }
-    fn get_flag_y(&self) -> bool {
+    pub fn get_flag_y(&self) -> bool {
         (self.f & 0x08) != 0
     }
-    fn get_flag_h(&self) -> bool {
+    pub fn get_flag_h(&self) -> bool {
         (self.f & 0x10) != 0
     }
-    fn get_flag_x(&self) -> bool {
+    pub fn get_flag_x(&self) -> bool {
         (self.f & 0x20) != 0
     }
-    fn get_flag_z(&self) -> bool {
+    pub fn get_flag_z(&self) -> bool {
         (self.f & 0x40) != 0
     }
-    fn get_flag_s(&self) -> bool {
+    pub fn get_flag_s(&self) -> bool {
         (self.f & 0x80) != 0
     }
 
     // == Flag pair helper functions == //
-    fn bc(&self) -> u16 {
+    pub fn bc(&self) -> u16 {
         ((self.b as u16) << 8) | (self.c as u16)
     }
 
-    fn set_bc(&mut self, val: u16) {
+    pub fn set_bc(&mut self, val: u16) {
         self.b = (val >> 8) as u8;
         self.c = val as u8;
     }
 
-    fn de(&self) -> u16 {
+    pub fn de(&self) -> u16 {
         ((self.d as u16) << 8) | (self.e as u16)
     }
 
-    fn set_de(&mut self, val: u16) {
+    pub fn set_de(&mut self, val: u16) {
         self.d = (val >> 8) as u8;
         self.e = val as u8;
     }
 
-    fn hl(&self) -> u16 {
+    pub fn hl(&self) -> u16 {
         ((self.h as u16) << 8) | (self.l as u16)
     }
 
-    fn set_hl(&mut self, val: u16) {
+    pub fn set_hl(&mut self, val: u16) {
         self.h = (val >> 8) as u8;
         self.l = val as u8;
     }
